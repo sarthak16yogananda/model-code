986af4e63bc35ba8caa11833c33f947a0f0c02cd
sarthak16yogananda <51529777+sarthak16yogananda@users.noreply.github.com>
2019-07-24 13:41:52 +0530

    Update and rename model.py to m.py

---
+++ m.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+from sklearn.metrics import explained_variance_score, \
+    mean_absolute_error, \
+    median_absolute_error
+from sklearn.model_selection import train_test_split
+df = pd.read_csv('weatherdata.csv').set_index('Date')
+df.describe().T
+df.info()
+df = df.drop(['MinTemp', 'MaxTemp'], axis=1)
+X = df[[col for col in df.columns if col != 'Temp3pm']]
+y = df['Temp3pm']
+X_train, X_tmp, y_train, y_tmp = train_test_split(X, y, test_size=0.2, random_state=23)
+X_test, X_val, y_test, y_val = train_test_split(X_tmp, y_tmp, test_size=0.5, random_state=23)
+X_train.shape, X_test.shape, X_val.shape
+print("Training instances   {}, Training features   {}".format(X_train.shape[0], X_train.shape[1]))
+print("Validation instances {}, Validation features {}".format(X_val.shape[0], X_val.shape[1]))
+print("Testing instances    {}, Testing features    {}".format(X_test.shape[0], X_test.shape[1]))
+feature_cols = [tf.feature_column.numeric_column(col) for col in X.columns]
+regressor = tf.estimator.DNNRegressor(feature_columns=feature_cols,
+                                      hidden_units=[50, 50],
+                                      model_dir='tf_wx_model')
+def wx_input_fn(X, y=None, num_epochs=None, shuffle=True, batch_size=400):
+    return tf.estimator.inputs.pandas_input_fn(x=X,
+                                               y=y,
+                                               num_epochs=num_epochs,
+                                               shuffle=shuffle,
+                                               batch_size=batch_size)
+regressor.train(input_fn=input_fn(training_data, num_epochs=None, shuffle=True), steps=some_large_number)
+regressor.evaluate(input_fn=input_fn(eval_data, num_epochs=1, shuffle=False), steps=1)
+predictions = regressor.predict(input_fn=input_fn(pred_data, num_epochs=1, shuffle=False), steps=1)
+evaluations = []
+STEPS = 400
+for i in range(100):
+    regressor.train(input_fn=wx_input_fn(X_train, y=y_train), steps=STEPS)
+    evaluations.append(regressor.evaluate(input_fn=wx_input_fn(X_val,
+                                                               y_val,
+                                                               num_epochs=1,
+                                                               shuffle=False)))
+(100 x 400 / 2) = 20,000 epochs
+evaluations[0]
+import matplotlib.pyplot as plt
+%matplotlib inline
+plt.rcParams['figure.figsize'] = [14, 10]
+loss_values = [ev['loss'] for ev in evaluations]
+training_steps = [ev['global_step'] for ev in evaluations]
+plt.scatter(x=training_steps, y=loss_values)
+plt.xlabel('Training steps (Epochs = steps / 2)')
+plt.ylabel('Loss (SSE)')
+plt.show()
+pred = regressor.predict(input_fn=wx_input_fn(X_test,
+                                              num_epochs=1,
+                                              shuffle=False))
+predictions = np.array([p['predictions'][0] for p in pred])
+
+print("The Explained Variance: %.2f" % explained_variance_score(
+                                            y_test, predictions))  
+print("The Mean Absolute Error: %.2f degrees Celcius" % mean_absolute_error(
+                                            y_test, predictions))  
+print("The Median Absolute Error: %.2f degrees Celcius" % median_absolute_error(
+                                            y_test, predictions))
